1fd711741e1bff0a52fb218ce616c8d86aed6440
brez7 <rob@barbank.com>
2024-02-28 13:56:39 -0800

    min

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ def menu():
 def contact():
     return render_template('contact.html', is_index=False)
 
+@app.route('/about')
+def about():
+    return render_template('about.html', is_index=False)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
